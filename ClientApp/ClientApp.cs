29dbfd3409e2b1be96fca16a752f03c9511a5898
Allen Zhang <Allen.Zhang@transmax.com.au>
2018-04-04 13:19:55 +1000

    first version of client server talking

---
+++ ClientApp/ClientApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using ServerClass;
+
+namespace ClientApp
+{
+    class ClientApp
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel chan = new TcpChannel();
+            ChannelServices.RegisterChannel(chan);
+
+            myRemoteClass obj = (myRemoteClass) Activator.GetObject(typeof(myRemoteClass), "tcp://localhost:8085/RemoteTest");
+
+            if (obj == null)
+            {
+                Console.WriteLine("Could not locate server");
+            }
+            else if (obj.SetString("Sending String to Server"))
+            {
+                System.Console.WriteLine("Success: Check the other console to verify.");
+            }
+            else
+            {
+                System.Console.WriteLine("Sending the test string has failed.");
+            }
+
+            Console.WriteLine("Hit <enter> to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RemoteServer/RemoteServer/RemoteServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace RemoteServer
+{
+    class RemoteServer
+    {
+        static void Main(string[] args)
+        {
+            // https://support.microsoft.com/en-us/help/307600/how-to-marshal-an-object-to-a-remote-server-by-reference-by-using-visu
+            TcpChannel chan = new TcpChannel(8085);
+            ChannelServices.RegisterChannel(chan);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                Type.GetType("ServerClass.myRemoteClass, ServerClass") ?? throw new InvalidOperationException(),
+                "RemoteTest",
+                WellKnownObjectMode.SingleCall);
+
+            Console.WriteLine("Hit <enter> to exit...");
+            Console.ReadLine();
+        }
+    }
+}
